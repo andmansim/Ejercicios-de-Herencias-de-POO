223d79a73825bf7f69dc12e9b4887b5884106711
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-19 20:06:45 +0100

    intento 1

--- herencia-simple/funciones.py
+++ herencia-simple/funciones.py
@@ -2,10 +2,13 @@ class Punto2D:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    X = x
+    Y = y
     
     def traslacion(self, a, b):
         self.x = self.x + a
         self.y = self.y + b
+        return self.x, self.y
 
 class Punto3D:
     def __init__(self, x, y, z):
@@ -19,7 +22,7 @@ class Punto3D:
         self.z = self.z + c
 
 a = Punto2D(1,5)
-print("A = {}".format(a))
+print(a)
 
-a.traslacion(-1, -2)
-print("A = {}".format(a))
+#a.traslacion(-1, -2)
+#print("A = {}".format(a))
