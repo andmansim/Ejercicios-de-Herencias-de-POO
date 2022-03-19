a33d03af2338701b3d2f50e3873e39a7a34e435b
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-19 20:50:44 +0100

    punto2D desde elo main

--- herencia-simple/main.py
+++ herencia-simple/main.py
@@ -8,3 +8,11 @@ if __name__ == "__main__":
     usuario_y = int(input())
     a = funciones.Punto2D(usuario_x, usuario_y) #a es un objeto de la clase, nos crea las coordenadas del punto
     print("A = X: {} ; Y: {}".format(a.x, a.y))
+    
+    print("Elija dos números que quiere sumar/restar a las coordenadas, para trasladarlo")
+    print("Trasladar X")
+    trasladar_x = int(input())
+    print("Trasladar Y")
+    trasladar_y = int(input())
+    a.traslacion(trasladar_x, trasladar_y)
+    print("A = X: {} ; Y: {}".format(a.x, a.y))   
