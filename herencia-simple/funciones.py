e518889259594c4261eb6dcab5105557e7f2d699
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-19 20:39:47 +0100

    heredamos la clase base

--- herencia-simple/funciones.py
+++ herencia-simple/funciones.py
@@ -9,12 +9,12 @@ class Punto2D:
         return self.x, self.y
 
 class Punto3D(Punto2D):
-    def __init__(self,z):
+    def __init__(self, x, y, z):
+    
+        Punto2D.__init__(self, x, y) #cogemos el constructor de la clase base
         self.z = z
     
-    def traslacion(self, a, b, c):
-        self.x = self.x + a
-        self.y = self.y + b
+    def traslacion(self, c):
         self.z = self.z + c
 
 a = Punto2D(1,5) #a es un objeto de la clase, nos crea las coordenadas del punto
@@ -22,3 +22,8 @@ print("A = X: {} ; Y: {}".format(a.x, a.y))
 
 a.traslacion(-1, -2)
 print("A = X: {} ; Y: {}".format(a.x, a.y))
+
+b = Punto3D(2, 5, 7)
+print("B = X: {} ; Y: {} ; Z: {} ".format(b.x, b.y, b.z))
+b.traslacion(1, 3, 2)
+print("A = X: {} ; Y: {}; Z: {} ".format(b.x, b.y, b.z))
