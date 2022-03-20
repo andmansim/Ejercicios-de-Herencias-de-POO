fbe5003fba383d1d5697294ec6487159b572fa5b
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-20 18:07:29 +0100

    clase B intento

--- herencia-mulriple/diamante-argumentos/funciones.py
+++ herencia-mulriple/diamante-argumentos/funciones.py
@@ -1,5 +1,11 @@
+from mimetypes import init
+
+
 class A:
     def __init__(self, a):
         self.a = a
 
-
+class B(class A):
+    def __init__(self, a, b):
+        class A.__init__(self, a)
+        self.b = B
