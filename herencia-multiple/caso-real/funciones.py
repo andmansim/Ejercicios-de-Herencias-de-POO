51c53f215a9ecd590396d24f42a4b265464c2c90
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-20 19:36:46 +0100

    primera parte hecha :)

--- herencia-multiple/caso-real/funciones.py
+++ herencia-multiple/caso-real/funciones.py
@@ -1,26 +1,28 @@
 class CASA:
-    def __init__(self, pared_n, pared_s, pared_e, pared_o, ventana_n, ventana_s, ventana_e, ventana_o):
-        self.pared_n = pared_n
-        self.pared_s = pared_s
-        self.pared_e = pared_e
-        self.pared_o = pared_o
+    def __init__(self, ventana_n, ventana_s, ventana_e, ventana_o):
         self.ventana_n = ventana_n
         self.ventana_s = ventana_s
         self.ventana_e = ventana_e
         self.ventana_o = ventana_o
+        
 
     def superficie_acristalada(self):
-        suma = self.ventana_n + self.ventana_s + self.ventana_e + self.ventana_o
+        suma = int(self.ventana_n) + int(self.ventana_s) + int(self.ventana_e) + int(self.ventana_o)
         return suma
 
-    def ventanas(self):
-        print("introduce la medida de la superficie de las ventanas")
-        print("ventana de la pared norte:")
-        self.ventana_n = input() #superficie de la ventana norte
-        print("ventana de la pared sur:") 
-        self.ventana_s = input()
-        print("ventana de la pared este:")
-        self.ventana_e = input()
-        print("ventana de la pared oeste:")
-        self.ventana_o = input()
 
+
+
+
+print("introduce la medida de la superficie de las ventanas")
+print("ventana de la pared norte:")
+v_n = input() #superficie de la ventana norte
+print("ventana de la pared sur:") 
+v_s = input()
+print("ventana de la pared este:")
+v_e = input()
+print("ventana de la pared oeste:")
+v_o = input()
+casa = CASA(v_n, v_s, v_e, v_o)
+suma = casa.superficie_acristalada()
+print(suma)
