d33c2f3d9430795e9cd93bd7b0c3a0fd9c66c20b
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-20 18:45:44 +0100

    main

---
+++ herencia-multiple/diamante-argumentos/main.py
@@ -0,0 +1,5 @@
+import funciones
+if __name__ == "__main__":
+    d = funciones.D(1, 2, 3)
+    print(isinstance(d, funciones.A),isinstance(d, funciones.B),isinstance(d, funciones.C))
+    print(d.a, d.b, d.c)    
