17aace65de6d99b5a4857d8ff18fcbba158e49c2
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-20 20:14:34 +0100

    codigo principal

--- herencia-multiple/caso-real/funciones.py
+++ herencia-multiple/caso-real/funciones.py
@@ -10,10 +10,27 @@ class CASA:
         suma = int(self.ventana_n) + int(self.ventana_s) + int(self.ventana_e) + int(self.ventana_o)
         return suma
 
+    
+def proteccion():
+    print("¿Le gutaria tener proteccion en las ventanas? s/n")
+    respuesta = input()
+    if respuesta == "s":
+        print("¿Cual es?")
+        opciones = ["persiana", "estor", "cortinas", "mosquitera"]
+        print(opciones)
+        cual_es = input()
+        if cual_es not in opciones:
+            print("de que vas tio, tienes un solo trabajo y lo haces mal")
+            proteccion()
+        else:
+            print("gracias. siguiente paso")
+    else:
+        print("pues aqui hemos acabado")
+        raise Exception("hasta luego")
 
 
-
-
+#calculo de las ventanas
+proteccion()
 print("introduce la medida de la superficie de las ventanas")
 print("ventana de la pared norte:")
 v_n = input()
@@ -28,6 +45,7 @@ casa = CASA(v_n, v_s, v_e, v_o)
 suma = casa.superficie_acristalada()
 print("Esta es la superficie total de las ventanas: " + str(suma))
 
+#calculo de las paredes cortina
 print("introduce la medida de la superficie de las paredes cortina")
 print("cortina norte:")
 c_n = input()
