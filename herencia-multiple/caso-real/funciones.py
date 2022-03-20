161af7ef57503676a6c016373b66d5f8295b9474
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-20 19:05:53 +0100

    casa clase

---
+++ herencia-multiple/caso-real/funciones.py
@@ -0,0 +1,7 @@
+class CASA:
+    def __init__(self, pared_n, pared_s, pared_e, pared_o):
+        self.pared_n = pared_n
+        self.pared_s = pared_s
+        self.pared_e = pared_e
+        self.pared_o = pared_o
+
